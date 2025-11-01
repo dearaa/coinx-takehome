b170e8cea7cacce8b805f2aa05ee1fa7e2676559
dearaa <107908141+dearaa@users.noreply.github.com>
2025-11-01 12:21:10 +0700

    Create pareto_trade_sell.sql

---
+++ pareto_trade_sell.sql
@@ -0,0 +1,39 @@
+with trade_summary as 
+(
+  SELECT 
+   *
+  FROM 
+  `third-pad-476804-p7.coinx.fact_trades` rtrade
+  where 
+    status = 'FILLED'
+    and side = 'SELL'
+),
+
+agg as 
+(
+  select
+    user_id,
+    round(sum(sum_trade_value),3) sum_trade_value
+  from trade_summary
+  group by 1
+),
+
+
+ranked as 
+(
+   SELECT
+    user_id,
+    sum_trade_value,
+    RANK() OVER (ORDER BY sum_trade_value DESC) AS rank_user,
+    SUM(sum_trade_value) OVER () AS total_all_trade,
+    round(SUM(sum_trade_value) OVER (ORDER BY sum_trade_value DESC),3) AS running_total
+  FROM agg
+)
+
+SELECT
+  user_id,
+  sum_trade_value,
+  running_total,
+  ROUND(100.0 * running_total / total_all_trade, 2) AS cumulative_percent
+FROM ranked
+ORDER BY sum_trade_value DESC;
